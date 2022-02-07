fa82604a294c454d25d59ca240654886acafc133
Volodymyr Yahello <vyahello@gmail.com>
2022-02-07 15:25:38 +0200

    Create basic jinja template

--- pypi_org/app.py
+++ pypi_org/app.py
@@ -3,10 +3,17 @@ import flask
 app = flask.Flask(__name__)
 
 
+def get_latest_packages():
+    return [
+        {'name': 'flask', 'version': '1.2.3'},
+        {'name': 'passlib', 'version': '2.4.0'},
+    ]
+
+
 @app.route('/')
 def index():
-    return 'Hello'
+    return flask.render_template('index.html', packages=get_latest_packages())
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
