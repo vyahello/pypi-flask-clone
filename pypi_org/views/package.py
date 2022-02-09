3e66dfc09ffed47f6e87776534dc5719a6117a4e
Volodymyr Yahello <vyahello@gmail.com>
2022-02-09 23:29:24 +0200

    Add sample of types param route

--- pypi_org/views/package.py
+++ pypi_org/views/package.py
@@ -1,11 +1,13 @@
 import flask
 
-from infra.view_modifiers import response
-
 blueprint = flask.Blueprint('packages', __name__, template_folder='templates')
 
 
 @blueprint.route('/project/<package_name>')
-@response(template_file='home/index.html')
-def package_details(package_name: str) -> dict:
-    return {'packages': ['Package details for {}'.format(package_name)]}
+def package_details(package_name: str) -> str:
+    return 'Package details for {}'.format(package_name)
+
+
+@blueprint.route('/<int:rank>')
+def popular(rank: int) -> str:
+    return '{}th most popular package'.format(rank)
