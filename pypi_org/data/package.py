f73278295339dcf7a64f8b728c3fdcd9a7528b8e
Volodymyr Yahello <vyahello@gmail.com>
2022-02-18 15:56:38 +0200

    Enhance package table

--- pypi_org/data/package.py
+++ pypi_org/data/package.py
@@ -1,3 +1,5 @@
+import datetime
+
 import sqlalchemy as sa
 
 from pypi_org.data.modelbase import SqlAlchemyBase
@@ -8,18 +10,18 @@ class Package(SqlAlchemyBase):
 
     # descriptor
     id = sa.Column(sa.String, primary_key=True)
-    created_date = sa.Column(sa.DateTime)
-    summary = sa.Column(sa.String)
-    description = sa.Column(sa.String)
+    created_date = sa.Column(sa.DateTime, default=datetime.datetime.now, index=True)
+    summary = sa.Column(sa.String, nullable=False)
+    description = sa.Column(sa.String, nullable=False)
 
     home_page = sa.Column(sa.String)
     docs_url = sa.Column(sa.String)
     package_url = sa.Column(sa.String)
 
     author_name = sa.Column(sa.String)
-    author_email = sa.Column(sa.String)
+    author_email = sa.Column(sa.String, index=True)
 
-    license = sa.Column(sa.String)
+    license = sa.Column(sa.String, index=True)
 
     def __repr__(self) -> str:
         return '<Package {}>'.format(self.id)
