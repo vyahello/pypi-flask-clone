f580330736df8c7f9fe1de222ea5e9e5b21ad615
Volodymyr Yahello <vyahello@gmail.com>
2022-02-21 14:45:37 +0200

    Complete sqalchemy db inserts script

--- pypi_org/bin/basic_inserts.py
+++ pypi_org/bin/basic_inserts.py
@@ -1,6 +1,14 @@
 import os
+import sys
+
+# Make it run more easily outside of PyCharm
+sys.path.insert(
+    0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..", ".."))
+)
+
 import pypi_org.data.db_session as db_session
-from data.package import Package
+from pypi_org.data.package import Package
+from pypi_org.data.releases import Release
 
 
 def main():
@@ -11,10 +19,31 @@ def main():
 
 def insert_a_package():
     p = Package()
-    p.id = input('Package ID')
-    p.summary = input('Package Summary')
-    p.author_name = input('Package Author')
-    p.license = input('Package License')
+    p.id = input('Package id / name: ').strip().lower()
+
+    p.summary = input("Package summary: ").strip()
+    p.author_name = input("Author: ").strip()
+    p.license = input("License: ").strip()
+
+    print("Release 1:")
+    r = Release()
+    r.major_ver = int(input("Major version: "))
+    r.minor_ver = int(input("Minor version: "))
+    r.build_ver = int(input("Build version: "))
+    r.size = int(input("Size in bytes: "))
+    p.releases.append(r)
+
+    print("Release 2:")
+    r = Release()
+    r.major_ver = int(input("Major version: "))
+    r.minor_ver = int(input("Minor version: "))
+    r.build_ver = int(input("Build version: "))
+    r.size = int(input("Size in bytes: "))
+    p.releases.append(r)
+
+    session = db_session.create_session()
+    session.add(p)
+    session.commit()
 
 
 def init_db():
--- pypi_org/data/db_session.py
+++ pypi_org/data/db_session.py
@@ -1,14 +1,15 @@
 import sqlalchemy as sa
 from sqlalchemy import orm
+from sqlalchemy.orm import Session
 
-from data.modelbase import SqlAlchemyBase
+from pypi_org.data.modelbase import SqlAlchemyBase
 
-factory = None
+__factory = None
 
 
 def global_init(db_file: str):
-    global factory
-    if factory:
+    global __factory
+    if __factory:
         return
 
     if not db_file or not db_file.strip():
@@ -17,9 +18,14 @@ def global_init(db_file: str):
     conn_str = f'sqlite:///{db_file.strip()}'
     print(f'Connecting to DB with {conn_str}')
     engine = sa.create_engine(conn_str, echo=False)
-    factory = orm.sessionmaker(bind=engine)
+    __factory = orm.sessionmaker(bind=engine)
 
     # noinspection PyUnresolvedReferences
     import pypi_org.data.__all_models  # noqa: F401
 
     SqlAlchemyBase.metadata.create_all(engine)
+
+
+def create_session() -> Session:
+    global __factory
+    return __factory()
